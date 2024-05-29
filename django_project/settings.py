5d9b6ba583abc668b1e2e2463ee9e9a3b2af987e
Jeremias <jeremontoya155@gmail.com>
2024-05-29 12:12:49 -0300

    Railways postgre

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,8 +1,13 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+import dj_database_url
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -67,10 +72,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
