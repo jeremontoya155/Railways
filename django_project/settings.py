3a5f33ecff797985c17c54ad6c7c8cc574f15321
Jeremias <jeremontoya155@gmail.com>
2024-05-29 11:10:44 -0300

    Railways cambios static

--- django_project/settings.py
+++ django_project/settings.py
@@ -106,7 +106,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -117,5 +116,14 @@ STATICFILES_DIRS=[
     
     os.path.join(BASE_DIR,'blog/static')
 ]
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blog', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 ALLOWED_HOSTS=['railways-production-0146.up.railway.app',]
