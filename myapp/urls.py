48ad79af293bc970f25066ec5ed409c30bb98155
Jeremias <jeremontoya155@gmail.com>
2024-06-07 10:07:57 -0300

    Actualizamos links para poder tener navbar general a

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path('', views.upload_file, name='upload_file'),
-    path('files/', views.file_list, name='file_list'),
-    path('files/<int:pk>/', views.file_detail, name='file_detail'),
+    path('', login_required(views.upload_file), name='upload_file'),
+    path('files/', login_required(views.file_list), name='file_list'),
+    path('files/<int:pk>/', login_required(views.file_detail), name='file_detail'),
 ]
